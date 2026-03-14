bbe517a1e720bb441f2088dac37069ede4e1dd28
Daham Dissanayake <dahamdissanayake05@gmail.com>
2026-03-14 19:11:06 +0530

    21

---
+++ Project 1/src/Simple_Cal.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Simple_Cal {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        //Num inputs
+        System.out.println("Enter first Num: ");
+        double num1 = input.nextDouble();
+        
+    }
+    
+    public 
+}
